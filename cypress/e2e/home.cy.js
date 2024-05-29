7f18b81f1c579c78b730ce18b49be334c605a7b1
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-29 15:20:48 +0200

    maj08

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,15 +1,27 @@
 /// <reference types="cypress" />
 
+import HomePage from '../pages/HomePage';
+
 describe('Home Page Tests', () => {
     beforeEach(() => {
         cy.login('admin', 'admin').then((response) => {
             expect(response.status).to.eq(200);
             localStorage.setItem('user', JSON.stringify(response.body));
         });
-        cy.visit('http://localhost:8081');
+        HomePage.visitHomePage();
     });
 
     it('should display at least one user on the home page', () => {
-        cy.get('ul').find('li').its('length').should('be.gte', 1);
+        HomePage.getUserListLength().should('be.gte', 1);
+    });
+
+    it('should navigate to login page on logout', () => {
+        HomePage.clickLogout();
+        cy.url().should('include', '/login');
+    });
+
+    it('should navigate to add user page when clicking add more users', () => {
+        HomePage.clickAddMoreUsers();
+        cy.url().should('include', '/add-user');
     });
 });
--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,4 +1,20 @@
 class HomePage {
+    visitHomePage() {
+        cy.visit('http://localhost:8081');
+    }
+
+    clickLogout() {
+        cy.get('#logout').click();
+    }
+
+    clickAddMoreUsers() {
+        cy.get('#addmore').click();
+    }
+
+    getUserListLength() {
+        return cy.get('ul').find('li').its('length');
+    }
+
     checkGreetingMessage(message) {
         cy.get('h1').should('contain', message);
     }
